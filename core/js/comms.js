fb932f637d3f0c3c3ea4057d873202b700530231
ysfss <61326539+ysfsvm@users.noreply.github.com>
2021-04-11 12:14:06 +0300

    Update comms.js

--- core/js/comms.js
+++ core/js/comms.js
@@ -27,7 +27,9 @@ const Comms = {
   // Gets a text command to append to what's being sent to show progress. If progress==undefined, it's the first command
   getProgressCmd : (progress) => {
     console.log(`<COMMS> getProgressCmd ${JSON.stringify(progress)}`);
-    if (!Const.HAS_E_SHOWMESSAGE) {
+    if {
+      return "Bluetooth.println("Uploading boot, skipping progress.");";
+    } else if (!Const.HAS_E_SHOWMESSAGE) {
       if (progress===undefined) return "p=x=>digitalPulse(LED1,1,10);";
       return "p();";
     } else {
