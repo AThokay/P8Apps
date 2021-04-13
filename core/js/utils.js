e27539e6bb097d89861486db9a0d0342a71f126d
ysfss <61326539+ysfsvm@users.noreply.github.com>
2021-04-13 14:54:13 +0300

    Update utils.js

--- core/js/utils.js
+++ core/js/utils.js
@@ -15,7 +15,7 @@ const Const = {
 
   /* base URL, eg https://github.com/${username}/BangleApps/tree/master/apps for
   links when people click on the GitHub link next to an app. undefined = no link*/
-  APP_SOURCECODE_URL : https://github.com/${username}/P8Apps/tree/main/apps,
+  APP_SOURCECODE_URL : undefined,
 
   /* Message to display when an app has been loaded */
   MESSAGE_RELOAD : 'Press button\nto reload -->',
