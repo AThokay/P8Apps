5a50e435c4877308d07303e201ccd2adbafa3a88
ysfss <61326539+ysfsvm@users.noreply.github.com>
2021-04-13 18:14:13 +0300

    Update comms.js

--- core/js/comms.js
+++ core/js/comms.js
@@ -27,13 +27,8 @@ const Comms = {
   // Gets a text command to append to what's being sent to show progress. If progress==undefined, it's the first command
   getProgressCmd : (progress) => {
     console.log(`<COMMS> getProgressCmd ${JSON.stringify(progress)}`);
-    if (!Const.HAS_E_SHOWMESSAGE) {
-      if (progress===undefined) return "p=x=>digitalPulse(LED1,1,10);";
-      return "p();";
-    } else {
       if (progress===undefined) return Const.CODE_PROGRESSBAR;
       return `p(${Math.round(progress*100)});`
-    }
   },
   getApp : (app) => {
 	    if (["boot","setting"].includes(app.id)) {
