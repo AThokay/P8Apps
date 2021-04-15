ee9353a1730583349b7e7eda106a051eab54a9e3
ysfss <61326539+ysfsvm@users.noreply.github.com>
2021-04-15 23:14:07 +0300

    Update torch.js

--- apps/torch/torch.js
+++ apps/torch/torch.js
@@ -1,12 +1,15 @@
-function torch(){
-        s.ontime = 600;
+function torch()
+{
 	g.reset();
 	g.fillRect(0,0,g.getWidth(),g.getHeight());
-        brightness(7);
 }
+
 TC.on('swipe',(dir)=>{
 	if (dir == TC.DOWN) load("launch.js");
 });
-setTimeout(()=>{
-	torch();
-},500);
+
+P8.ON_TIME=600;
+
+setTimeout(()=>{P8.sleep();},100);
+setTimeout(()=>{P8.wake();brightness(7);},100);
+setTimeout(()=>{torch();},100);
