83ae4feec99bc7254b5cf59cdd7b582779f05206
ysfss <61326539+ysfsvm@users.noreply.github.com>
2021-04-15 23:00:13 +0300

    Update file-icon.js

--- apps/file/file-icon.js
+++ apps/file/file-icon.js
@@ -1,1 +1,1 @@
-require("heatshrink").decompress(atob("mEwxH+AH4A/ADOCADQuuGCQudGCIvz2YAV1ovu2Yv/F/+BAAwv/R/4v/F/4vPADYv/X/4v/F/4vLwIAGF/6P/F/4v/F54AbF/6//F/4v/F5eBAAwv/R/4v/F7+tFyutF64AbF/4weFyAwdFyQACqwACCcYA/ADY="))
+require("heatshrink").decompress(atob("mEwghC/AEkIxAABwUiAAwKBC6+AC6ERiIXDGBAXPGA8JzIAByQXKGA4XUA4eDmYAGJwQXVxEizAXPIgIXDwWZC6uIxIwCC6eIGAQX/C9i/FC5mCCw0yC5wAMC/4Xnx//ABf4C/Xzdw8zn4XkL/5f/L+oUDI6YX3AB4XeAH4AdA=="))
