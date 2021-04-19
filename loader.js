1f55cf9966a2997fb7fde2c962eec479c8d4e097
ysfss <61326539+ysfsvm@users.noreply.github.com>
2021-04-20 01:09:53 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -4,11 +4,3 @@
   if (githubMatch) username = githubMatch[1];
   Const.APP_SOURCECODE_URL = `https://github.com/${username}/P8Apps/tree/master/apps`;
 })();
-
-function onFoundDeviceInfo(deviceId, deviceVersion) {
-  if (deviceId != "P8") {
-    showToast(`You're using ${deviceId}, not a P8. Did you want <a href="https://espruino.com/apps">espruino.com/apps</a> instead?` ,"warning", 20000);
-  } else if (versionLess(deviceVersion, RECOMMENDED_VERSION)) {
-    showToast(`You're using an old Bangle.js firmware (${deviceVersion}). You can <a href="https://www.espruino.com/Bangle.js#firmware-updates" target="_blank">update with the instructions here</a>` ,"warning", 20000);
-  }
-}
